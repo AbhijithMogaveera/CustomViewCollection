38cd55ffed432895d06073dc3e5a3157368d96fe
Abhijith Mogaveera <abhialur8898@gmail.com>
2022-01-31 00:24:42 +0530

    MiuiCylinderGraph

---
+++ miui_cylinder_graph/src/main/java/com/abhijith/miui_cylinder_graph/model/SectionDataWrapper.kt
@@ -0,0 +1,8 @@
+package com.abhijith.miui_cylinder_graph.model
+
+import android.graphics.Rect
+
+data class SectionDataWrapper(
+    val data:SectionData,
+    val rect: Rect = Rect()
+)
--- miui_cylinder_graph/src/main/java/com/abhijith/miui_cylinder_graph/ui/CylinderGraphView.kt
+++ miui_cylinder_graph/src/main/java/com/abhijith/miui_cylinder_graph/ui/CylinderGraphView.kt
@@ -5,10 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.abhijith.miui_cylinder_graph.data.DummyData
 import com.abhijith.miui_cylinder_graph.extension.getSpaceDistributed
+import com.abhijith.miui_cylinder_graph.model.SectionData
+import com.abhijith.miui_cylinder_graph.model.SectionDataWrapper
 import com.abhijith.miui_cylinder_graph.shape.CurvyRectangleDrawable
 import com.abhijith.miui_cylinder_graph.shape.CylinderDrawable
 import com.abhijith.miui_cylinder_graph.util.ViewHelper
@@ -19,27 +22,56 @@ class CylinderGraphView @JvmOverloads constructor(
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes), ViewHelper {
-
+    private val shrinkY = 110
     private val cylinderDrawable = CylinderDrawable()
     private val curvyRectangleDrawable = CurvyRectangleDrawable()
+
+    private var sectionListWithWrapper: List<SectionDataWrapper> = listOf()
+
+    init {
+        setData(DummyData.items)
+    }
+
+    private fun setData(list: List<SectionData>) {
+        sectionListWithWrapper = list.map {
+            SectionDataWrapper(it)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        val start = paddingStart
+        val top = paddingTop
+        canvasBound.set(start, top, w, h)
+        canvasBound.bottom = canvasBound.bottom - shrinkY
+        calculateIndividualSectionData()
+    }
+
+    private fun calculateIndividualSectionData() {
+        canvasBound.getSpaceDistributed(
+            this,
+            sectionListWithWrapper.map {
+                it.data
+            }
+        ) { index, rect, sectionData ->
+            rect.bottom = rect.bottom + shrinkY
+            sectionListWithWrapper[index].rect.set(rect)
+        }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas ?: return
-        canvas.getClipBounds(canvasBound)
-        val i = 110
-        canvasBound.bottom = canvasBound.bottom - i
-        canvasBound.getSpaceDistributed(this, DummyData.items) { index, rect, sectionData ->
-            rect.bottom = rect.bottom + i
+        sectionListWithWrapper.forEachIndexed { index, sectionDataWrapper ->
             if (index == 0) {
-                cylinderDrawable.bounds = rect
+                cylinderDrawable.bounds = sectionDataWrapper.rect
                 cylinderDrawable.reset()
-                cylinderDrawable.color = ContextCompat.getColor(context, sectionData.color)
+                cylinderDrawable.color = ContextCompat.getColor(context, sectionDataWrapper.data.color)
                 cylinderDrawable.draw(canvas)
             } else {
-                curvyRectangleDrawable.bounds = rect
+                curvyRectangleDrawable.bounds = sectionDataWrapper.rect
                 curvyRectangleDrawable.reset()
-                curvyRectangleDrawable.color = ContextCompat.getColor(context, sectionData.color)
+                curvyRectangleDrawable.color = ContextCompat.getColor(context, sectionDataWrapper.data.color)
                 curvyRectangleDrawable.draw(canvas)
             }
         }
